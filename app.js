d8b51b2a7d29a43e93591d6b3687e686513a798c
Alex48611 <Alex4861@web.de>
2025-06-03 17:59:08 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-// JavaScript Placeholder
-console.log('App gestartet');
+
+// Einstiegskurs speichern (Platzhalter-Logik)
+function speichern() {
+    const kurs = document.getElementById('einkaufspreis').value;
+    if (kurs) {
+        localStorage.setItem('einstiegskurs', kurs);
+        document.getElementById('meldung').innerText = `Einstiegskurs ${kurs} gespeichert.`;
+    } else {
+        document.getElementById('meldung').innerText = 'Bitte einen Kurs eingeben.';
+    }
+}
